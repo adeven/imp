3c3ac03a981bbc45357726e2ff5006a51bc1b664
Robert Abraham <robert@adeven.com>
2014-01-16 15:34:31 +0100

    added run function

--- lib/Imp.pm
+++ lib/Imp.pm
@@ -2,7 +2,7 @@ package Imp;
 
 require Exporter;
 @ISA    = qw(Exporter);
-@EXPORT = qw(include);
+@EXPORT = qw(include run);
 
 use strict;
 use warnings;
@@ -12,12 +12,16 @@ use Imp::System;
 use Imp::Template;
 
 our $VERSION = '0.0.1';
-our $HOME    = '/var/lib/imp/current';
 
 sub include {
     my $include = shift;
     eval { system("./bin/$include") == 0 or die $!; };
     print $@ if $@;
+}
+
+sub run {
+    my $cmd = shift;
+    system($cmd ) == 0 or die $!;
 }
 
 1;
